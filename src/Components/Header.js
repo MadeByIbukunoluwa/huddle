c1700aae22d6e8bf7e49db2de97851bb0f25ec93
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-08-30 23:32:44 +0100

    solved image directory issue

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { StyledHeader } from './styles/Header.styled'
+import { StyledHeader, Nav, Logo,Image } from "./styles/Header.styled";
+import { Container } from './styles/Container.styled'
+import { Button } from './styles/Button.styled'
+import { Flex } from './styles/flex.styled'
 
 const Header = () => {
   return (
     <StyledHeader>
-        <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          {/* <Logo src="../../public/Images/logo.svg" alt = ""/> */}
+          {/* <Logo src={process.env.PUBLIC_URL + '/Images/logo.svg'}alt = ""/> */}
+          <Logo src="/Images/logo.svg" alt = ""/>
+          
+          <Button>Try it free </Button>
+        </Nav>
+
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p> Huddle re-imagines the way we build communities.You have a voice, but so deos your audience. Create connections with your users as you engage in genuine discussions 
+            </p>
+              <Button bg = "#ff0099" color= "fff">Get Started For Free</Button>
+          </div>
+          <Image src = "/Images/illustration-mockups.svg" alt = ""/>
+        </Flex>
+      </Container>
     </StyledHeader>
-  )
+  );
 }
 
 export default Header
