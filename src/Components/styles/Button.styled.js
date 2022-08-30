d01790b160509943f5ab6b44a428a6a968f01903
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-08-30 23:33:27 +0100

    debugged multiple components

--- src/Components/styles/Button.styled.js
+++ src/Components/styles/Button.styled.js
@@ -4,12 +4,18 @@ import styled from "styled-components";
 
 
 export const Button = styled.button`
-border-radius:50px;
-border:none;
-box-shadow:0 0 10px rgba(0, 0, 0, 0.15);
-cursor:pointer;
-
-
-
-
-`
+  border-radius: 50px;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 15px 60px;
+  margin-top: 10px;
+  background-color: ${({ bg }) => bg || "#fff"};
+  color: ${({ color }) => color || "#333"};
+  &:hover {
+    opacity: 0.9;
+    transform: scale(0.9);
+  }
+`;
--- src/Components/styles/Header.styled.js
+++ src/Components/styles/Header.styled.js
@@ -5,12 +5,30 @@ export const StyledHeader = styled.header`
     background-color:${({theme}) => theme.colors.header};
     padding: 40px 0 ;
 
-    h1 {
-        color:red
-    }
-
-    &:hover {
-        background-color:black;
-    }
 `
+
+export const Nav = styled.nav`
+        display:flex;
+        align-items:center;
+        justify-content:space-between;
+        margin-bottom:40px;
+        @media screen and (max-width:${({theme}) => theme.mobile}) {
+            flex-direction:column;
+        }
+`
+export const Logo = styled.img`
+  @media screen and (max-width: ${({ theme }) => theme.mobile}) {
+    margin-bottom:40px;
+  }
+`;
+
+
+export const Image = styled.img`
+  width: 375px;
+  margin-left: 40px;
+  @media screen and (max-width: ${({ theme }) => theme.mobile}) {
+    margin: 40px 0 30px;
+  }
+`;
+
 
