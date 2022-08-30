fae2bab7a03740aaeb0d2b8956b3eb2874068887
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-08-30 18:36:25 +0100

    added Theme provider with theme object

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { ThemeProvider } from "styled-components";
 import { Container } from "./Components/styles/Container.styled";
 import Header from "./Components/Header";
 function App() {
+  const theme = {
+    colors : {
+      header:"#ebfbff",
+      body:"#fff",
+      footer:"#003333",
+    }
+  }
   return (
+    <ThemeProvider theme={theme} > 
     <>
 <Header/>
 <Container>
   <h1>Hello World </h1>
 </Container>
     </>
+    </ThemeProvider>
   );
 }
 
