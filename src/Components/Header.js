1c4d5cc63478e75fcd7a76f65e9f2764a0915e38
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-08-30 18:13:21 +0100

    changed div tag to styledHeader and added its corresponding import

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
 
 const Header = () => {
   return (
-    <div>
+    <StyledHeader>
         <h1>Hubble</h1>
-    </div>
+    </StyledHeader>
   )
 }
 
